85e123fc14522b0e46f8e44af8bff920f39376ac
Luis Ardila <luis.ardila@kit.edu>
2021-01-15 12:22:06 +0100

    small typo in top level

--- src/example_IBERT_MGT_16G_25G.v
+++ src/example_IBERT_MGT_16G_25G.v
@@ -68,7 +68,7 @@ module example_IBERT_MGT_16G_25G
     
    wire [ 0 : C_SLV_DWIDTH - 1] heater_read_add;
    wire [ 0 : C_SLV_DWIDTH - 1] heater_output;
-   wire [31 : 0 ] heater_adjus;
+   wire [31 : 0 ] heater_adjust;
    wire heater_reset;
    wire heater_enable;
   //
