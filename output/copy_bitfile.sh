5fccfecd6bd51e2d7717901419b2fda695c732b2
Luis Ardila <luis.ardila@kit.edu>
2021-01-19 10:29:37 +0100

    bin file

--- output/copy_bitfile.sh
+++ output/copy_bitfile.sh
@@ -1,2 +1,2 @@
-cp ../Serenity-A2577_IBERT/Serenity-A2577_IBERT.runs/impl_1/*.ltx ../Serenity-A2577_IBERT/Serenity-A2577_IBERT.runs/impl_1/*.bit .
+cp ../Serenity-A2577_IBERT/Serenity-A2577_IBERT.runs/impl_1/*.ltx ../Serenity-A2577_IBERT/Serenity-A2577_IBERT.runs/impl_1/*.bit ../Serenity-A2577_IBERT/Serenity-A2577_IBERT.runs/impl_1/*.bin .
 
